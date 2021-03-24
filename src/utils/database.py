3206497a70428555ecf94f286787a1a62cf926fb
vcokltfre <vcokltfre@gmail.com>
2021-03-24 18:29:26 +0000

    feat: add some guild methods to db

--- src/utils/database.py
+++ src/utils/database.py
@@ -32,3 +32,9 @@ class Database:
     async def fetch(self, query: str, *args):
         async with self.pool.acquire() as conn:
             return await conn.fetch(query, *args)
+
+    async def fetch_guild(self, id: int):
+        return await self.fetchrow("SELECT * FROM Guilds WHERE id = $1;", id)
+
+    async def create_guild(self, id: int, owner_id: int) -> bool:
+        await self.execute("INSERT INTO Guilds (id, owner_id) VALUES ($1, $2);", id, owner_id)
