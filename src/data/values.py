b6884dafddda23cdbd711393889ed312c5bb708e
vcokltfre <vcokltfre@gmail.com>
2021-03-24 18:55:39 +0000

    feat: add config value validator

--- src/data/values.py
+++ src/data/values.py
@@ -2,3 +2,9 @@ ALGOS = {
     "linear": 0,
     "quadratic": 1,
 }
+
+LEVELUP_TYPES = {
+    "react": 0,
+    "chat": 1,
+    "dms": 2,
+}
---
+++ src/utils/validator.py
@@ -0,0 +1,38 @@
+from collections import namedtuple
+
+from src.data.values import ALGOS, LEVELUP_TYPES
+
+
+Result = namedtuple('Result', ["value", "valid", "reason"])
+
+
+class ConfigValidator:
+    @staticmethod
+    def validate_default_xp(xp: int) -> Result:
+        if 10 <= xp <= 1000:
+            return Result(xp, True, None)
+        return Result(None, False, "Value for default XP gain must be between 10 and 1,000.")
+
+    @staticmethod
+    def validate_algorithm(algo: str) -> Result:
+        if algo := ALGOS.get(algo.lower()):
+            return Result(algo, True, None)
+        return Result(None, False, f"Value for algorithm must be one of: {', '.join(list(ALGOS.keys()))}")
+
+    @staticmethod
+    def validate_levelup_type(type: str) -> Result:
+        if type := LEVELUP_TYPES.get(type.lower()):
+            return Result(type, True, None)
+        return Result(None, False, f"Value for levelup type must be one of {', '.join(list(LEVELUP_TYPES.keys()))}")
+
+    @staticmethod
+    def validate_levelup_message(msg: str) -> Result:
+        if 0 < len(msg) <= 1024:
+            return Result(msg, True, None)
+        return Result(None, False, "Value for level up message must be between 1 and 1024 characters.")
+
+    @staticmethod
+    def validate_level_role(value: str) -> Result:
+        if value in ["on", "off"]:
+            return Result(True if value == "on" else False, True, None)
+        return Result(None, False, "Value for level role setting must be on or off.")
