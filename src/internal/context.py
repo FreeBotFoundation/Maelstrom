9f00976c877124d52375d7bd04be4828a26f08e5
vcokltfre <vcokltfre@gmail.com>
2021-03-24 18:41:49 +0000

    feat: add fetch guild method to context

--- src/internal/context.py
+++ src/internal/context.py
@@ -4,4 +4,5 @@ from discord.ext.commands import Context as _BaseContext
 class Context(_BaseContext):
     """A Custom Context for extra functionality."""
 
-    pass # Use to extend context functionality
+    async def fetch_guild(self):
+        return await self.bot.db.fetch_guild(self.guild)
