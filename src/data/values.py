aca6022cfe38b3758a778ba8de25869fa8af0057
vcokltfre <vcokltfre@gmail.com>
2021-03-24 19:23:12 +0000

    feat: add reverse types to values

--- src/data/values.py
+++ src/data/values.py
@@ -1,10 +1,16 @@
+def rd(d: dict): return {v: k for k, v in d.items()}
+
 ALGOS = {
     "linear": 0,
     "quadratic": 1,
 }
+
+ALGOS_REVERSE = rd(ALGOS)
 
 LEVELUP_TYPES = {
     "react": 0,
     "chat": 1,
     "dms": 2,
 }
+
+LEVELUP_TYPES_REVERSE = rd(LEVELUP_TYPES)
